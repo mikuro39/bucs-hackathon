255c9f89937c976aa12d860d64eb9557538b2019
m20arcusk <marcuskam20@gmail.com>
2023-03-18 19:30:13 -0700

    Updated Front end with working button

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 var rizzbutton = document.querySelector('.rizzbutton')
-var rizzmsg = document.querySelector('.message');
+var rizzmsg = document.querySelector('#message');
 var rizzsong = new Audio("rizzsong.mp3");
 
-rizzbutton.addEventListener("click", function(){
-    if (rizzsong.paused){
-        document.body.style.backgroundImage = "url('gigachad.png')";
+const textInput = document.getElementById('text')
+const speedInput = document.getElementById('speed')
+const pauseInput = document.getElementById('pause-button')
+const stopInput = document.getElementById('stop-button')
+const playButton = document.getElementById('play-button')
 
+rizzbutton.addEventListener("click", function () {
+    if (rizzsong.paused) {
+        document.body.style.backgroundImage = "url('gigachad.png')";
         rizzsong.currentTime = 0;
+        rizzsong.volume = 0.20;
         rizzsong.play();
+        playText(textInput.value);
         rizzmsg.classList.toggle('reveal');
-    }else{
+    } else {
         rizzsong.pause();
-        document.body.style.backgroundImage='none';
+        document.body.style.backgroundImage = 'none';
+        rizzmsg.classList.toggle('reveal');
     }
 });
 
-rizzbutton.addEventListener("click", function(){
+rizzbutton.addEventListener("click", function () {
 
 });
+
+/*
+rizzbutton.addEventListener('click', () =>{
+   
+})
+*/
+
+function playText(text) {
+    const utterance = new SpeechSynthesisUtterance('Hey Troy, are you 5 cans of Monster? Because you make me shake uncontrollably and keep me up all night.')
+    speechSynthesis.speak(utterance);
+}
